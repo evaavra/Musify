2b134987bc229c33e8885f1cbc79e18e08d0b44d
Marios Thiraios <mariosthiraios@gmail.com>
2022-09-07 16:05:44 +0300

    ArtistsApiController

---
+++ Musify/Areas/Admin/Controllers/api/ArtistsController.cs
@@ -0,0 +1,35 @@
+﻿using Musify.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Musify.Areas.Admin.Controllers.api
+{
+    public class ArtistsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtistsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            var artist = _context.Artists.Single(a => a.ID == id);
+            if (artist == null)
+                return NotFound();
+            _context.Artists.Remove(artist);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
