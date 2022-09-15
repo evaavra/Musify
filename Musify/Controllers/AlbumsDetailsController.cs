5bf3a330075e3094d749d33ac16e32cf6b55ad29
Eva Avramidou <eva_avra5@yahoo.gr>
2022-09-15 15:40:18 +0300

    Albums Details

---
+++ Musify/Controllers/AlbumsDetailsController.cs
@@ -0,0 +1,49 @@
+﻿using Musify.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Musify.ViewModels;
+
+namespace Musify.Controllers
+{
+    public class AlbumsDetailsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public AlbumsDetailsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: AlbumsDetails
+        public ActionResult Index()
+        {
+            var viewmodel = new AlbumsIndexViewModel()
+            {
+                Albums = _context.Albums.Include(a => a.Songs).ToList()
+            };
+            return View(viewmodel);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = _context.Albums
+                .Include(a => a.Songs)
+                .SingleOrDefault(a => a.ID == id);
+
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+    }
+}
---
+++ Musify/ViewModels/AlbumsIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Musify.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Musify.ViewModels
+{
+    public class AlbumsIndexViewModel
+    {
+        public List<Album> Albums { get; set; }
+    }
+}
