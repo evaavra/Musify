0d6b30f768a1bd1f48fc515b4b4e67cde16c4e89
Eva Avramidou <eva_avra5@yahoo.gr>
2022-09-17 23:53:02 +0300

    SongsDetails

---
+++ Musify/Controllers/SongsDetailsController.cs
@@ -0,0 +1,47 @@
+﻿using Musify.Interfaces;
+using Musify.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Musify.Controllers
+{
+    public class SongsDetailsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SongsDetailsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        // GET: SongsDetails
+        public ActionResult Index(string query = null)
+        {
+            var songs = _unitOfWork.Songs.GetAllWithAlbumAndArtist();
+
+            if (!String.IsNullOrEmpty(query))
+            {
+                songs = songs
+                    .Where(s =>
+                        s.Title.Contains(query) ||
+                        s.Album.Artist.Name.Contains(query));
+            }
+
+            var viewmodel = new SongsDetailsViewModel()
+            {
+                Songs = songs.ToList(),
+                SearchTerm = query
+            };
+
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public ActionResult Search(SongsDetailsViewModel viewmodel)
+        {
+            return RedirectToAction("Index", "SongsDetails", new { query = viewmodel.SearchTerm });
+        }
+    }
+}
---
+++ Musify/ViewModels/SongsDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Musify.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Musify.ViewModels
+{
+    public class SongsDetailsViewModel
+    {
+        public List<Song> Songs { get; set; }
+
+        public string SearchTerm { get; set; }
+    }
+}
